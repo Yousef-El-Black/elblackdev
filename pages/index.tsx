ddcebb4ad919b107340d61f9781abcf5844d11a4
Yousef-El-Black <yousefelblackdev@gmail.com>
2023-05-11 16:45:55 +0300

    Upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,9 @@
-import Button from "@/components/Button";
 import Head from "next/head";
-import { useEffect } from "react";
-import KUTE from "kute.js";
 import Header from "@/components/Header";
 import Home from "@/components/Home";
 import About from "@/components/About";
 
 export default function Index() {
-  // useEffect(() => {
-  //   const tween = KUTE.fromTo(
-  //     "#blob1", // selector for first blob
-  //     { path: "#blob1" }, // from state
-  //     { path: "#blob2" }, // to state
-  //     { repeat: 999, duration: 2000, yoyo: true } // options
-  //   );
-  //   tween.start();
-  // }, []);
-
   return (
     <>
       <Head>
